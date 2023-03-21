2d0b2db5eeb7e948184a40d744404c5293e1c8b9
GGravys-S <simeon.gravis@insa-cvl.fr>
2023-03-21 17:05:14 +0100

    First commit : copy of old terminus project + new file treatment system

---
+++ Model/terrain.py
@@ -0,0 +1,37 @@
+class Terrain:
+    def __init__(self, x, y, id_t):
+        self.nbr_cases = 1
+        self.pos_x = x  # position x sur la map
+        self.pos_y = y  # position y sur la map
+        self.name = 'Terrain vide'
+        self.id = id_t  # id du terrain:wq
+        self.texture = ""
+        self.id_bat = self.id
+
+    def ret_coord(self):
+        return (self.pos_x, self.pos_y)
+
+class Water(Terrain):
+    def __init__(self, posx, posy):
+        Terrain.__init__(self, posx, posy, 1)
+        self.name = "Water"
+
+
+class Rock(Terrain):
+    def __init__(self, posx, posy):
+        Terrain.__init__(self, posx, posy, 2)
+        self.name = "Rock"
+
+
+class Enter_Pannel(Terrain):
+    def __init__(self, posx, posy):
+        Terrain.__init__(self, posx, posy, 115)
+        self.name = "Enter_Pannel"
+        self.texture = "direction1"
+
+
+class Exit_Pannel(Terrain):
+    def __init__(self, posx, posy):
+        Terrain.__init__(self, posx, posy, 116)
+        self.name = "Exit_Pannel"
+        self.texture = "direction2"
